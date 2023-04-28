7a10a32673b9bd919668db245ee1d3b05566ff34
Otabek Nurmatov <otabekswe@Mac.local>
2023-04-28 15:25:48 +0500

    Fixed: Language menu link

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fc6014a35ae413c3"; }
+/******/ 		__webpack_require__.h = function() { return "ca7ae5f1b5392bcd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
