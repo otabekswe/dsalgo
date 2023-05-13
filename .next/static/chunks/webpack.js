d2a7990efa514b3e8d483598b737926fcb1d546c
Otabek Nurmatov <otabekswe@Mac.local>
2023-05-14 01:07:58 +0500

    Updated: Filled with content and fixed some bugs

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bc79af117666fbd4"; }
+/******/ 		__webpack_require__.h = function() { return "106a9b4871ed58d5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
